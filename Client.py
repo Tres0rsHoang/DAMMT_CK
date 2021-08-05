5cf87bc50c94b33b4213658eaf8f2c3b7443bb44
Hoàng Bảo <baokyo002@gmail.com>
2021-08-06 00:00:56 +0700

    Bao - Dang Nhap Dang Ki server

--- Client.py
+++ Client.py
@@ -1,6 +1,67 @@
 import socket
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
+from tkcalendar import *
+
+def Show():
+	newwin = Tk()
+
+	option = [
+	            "AUSTRALIAN DOLLAR",
+	            "CANADIAN DOLLAR",
+	            "SWISS FRANC",
+	            "YUAN RENMINBI",
+	            "DANISH KRONE",
+	            "EURO",
+	            "POUND STERLING",
+	            "HONGKONG DOLLAR",
+	            "INDIAN RUPEE",
+	            "YEN",
+	            "KOREAN WON",
+	            "KUWAITI DINAR",
+	            "MALAYSIAN RINGGIT",
+	            "NORWEGIAN KRONER",
+	            "RUSSIAN RUBLE",
+	            "SAUDI RIAL",
+	            "SWEDISH KRONA",
+	            "SINGAPORE DOLLAR",
+	            "THAILAND BAHT",
+	            "US DOLLAR",
+	        ]
+
+	SelectDay = Label(
+	   newwin, 
+	   text="Ngày tra cứu",
+	   ).grid(row=0, column=0, pady = 5)
+	cal = Calendar(newwin, selectmode = "day", year = 2021, month = 8, day = 5)
+	cal.grid(row=1, column = 0)
+	SelectUnit = Label(
+	   newwin, 
+	   text="Loại tiền tệ",
+	   ).grid(row=2, column=0, pady = 5)
+
+	Unit = ttk.Combobox(
+	    newwin,
+	    width = 22,
+	    values = option
+	    )
+	Unit.insert(0, "Chọn loại tiền tệ")
+	Unit.grid(row=3,column=0)
+
+	def Confirm():
+	   Day = cal.get_date()
+	   Money = Unit.get()
+
+	   print(Day, Money)
+
+	ConfirmButton = Button(
+	   newwin,
+	   text="Tra Cứu",
+	   height = 10,
+	   width = 10,
+	   command = Confirm
+	   ).grid(row=1, column=1)
 
 def Connected():
 	newwin = Tk()
@@ -29,28 +90,134 @@ def Connected():
 	Passwords_Entry.insert(END, "Password")
 	Passwords_Entry.grid(row=3,column=0)
 
+
+	def DN():
+		client.sendall(bytes("DangNhap","utf8"))
+		check = client.recv(1024).decode("utf8")
+
+		Username = Username_Entry.get()
+		Passwords = Passwords_Entry.get()
+
+		client.sendall(bytes(Username,"utf8"))
+		check = client.recv(1024).decode("utf8")
+
+		client.sendall(bytes(Passwords,"utf8"))
+		check = client.recv(1024).decode("utf8")
+
+
+		Mess = client.recv(1024).decode("utf8")
+		client.sendall(bytes("OK","utf8"))
+
+		if Mess == "WrongPass":
+			messagebox.showinfo("Thông báo", "Sai mật khẩu")
+			return
+		if Mess == "WrongUser":
+			messagebox.showinfo("Thông báo", "Sai Username")
+			return
+		if Mess == "RightUser":
+			messagebox.showinfo("Thông báo", "Đăng nhập thành công!!!")
+			newwin.destroy()
+			Show()
+			return
+
+
 	LogIn = Button(
 	   newwin,
 	   width = 10,
-	   text = "Đăng nhập"
+	   text = "Đăng nhập",
+	   command = DN
 	   )
 	LogIn.grid(row=4,column=0,pady = 10)
+
+	def DK():
+		newwin2 = Tk()
+		newwin2.title("Đăng Kí")
+
+		TextUsername = Label(
+		   newwin2,
+		   text = "Username"
+		   ).grid(row=0,column=0,sticky=W)
+		Username_Entry = Entry(
+		   newwin2,
+		   width = 60,
+		   )
+		Username_Entry.insert(END, "Tên đăng nhập")
+		Username_Entry.grid(row=1,column=0, padx = 20)
+
+		TextPassword = Label(
+		   newwin2,
+		   text = "Password"
+		   ).grid(row=2,column=0,sticky=W)
+		Passwords_Entry = Entry(
+		   newwin2,
+		   show = '*',
+		   width = 60,
+		   )
+		Passwords_Entry.insert(END, "Password")
+		Passwords_Entry.grid(row=3,column=0)
+
+		TextConfirmPassword = Label(
+		   newwin2,
+		   text = "Confirm Password"
+		   ).grid(row=4,column=0,sticky=W)
+		ConfirmPasswords_Entry = Entry(
+		   newwin2,
+		   show = '*',
+		   width = 60,
+		   )
+		ConfirmPasswords_Entry.insert(END, "Password")
+		ConfirmPasswords_Entry.grid(row=5,column=0)
+
+		def SendDK():
+			ConfirmPasswords = ConfirmPasswords_Entry.get()
+			Passwords = Passwords_Entry.get()
+			Username = Username_Entry.get()
+			if Username.find(" ") != -1: 
+				messagebox.showinfo("Thông báo", "Không được để khoảng trắng trong USERNAME")
+				return
+			if ConfirmPasswords != Passwords:
+				messagebox.showinfo("Thông báo", "Mật khẩu không trùng khớp")
+				return
+
+			client.sendall(bytes("DangKi","utf8"))
+			check = client.recv(1024).decode("utf8")
+
+			client.sendall(bytes(Username,"utf8"))
+			check = client.recv(1024).decode("utf8")
+
+			ExistUsername = client.recv(1024).decode("utf8")
+			client.sendall(bytes("OK","utf8"))
+
+			if ExistUsername == "ExistUsername":
+				messagebox.showinfo("Thông báo", "Username đã tồn tại, vui lòng chọn username khác")
+				return
+			if ExistUsername == "ValidUsername":
+				client.sendall(bytes(Passwords,"utf8"))
+				check = client.recv(1024).decode("utf8")
+				messagebox.showinfo("Thông báo", "Chúc mừng bạn đã đăng kí thành công!!!")
+				newwin2.destroy()
+				return
+
+		SignUp = Button(
+		   newwin2,
+		   width = 10,
+		   text = "Đăng kí",
+		   command = SendDK
+		   )
+		SignUp.grid(row=6,column=0,pady = 10)
 
 	TextRegis = Label(
 	   newwin,
 	   text = "Nếu chưa có tài khoản, vui lòng ấn đăng ký"
 	   ).grid(row=5,column=0)
-
 	Regis = Button(
 	   newwin,
 	   width = 10,
-	   text = "Đăng kí"
+	   text = "Đăng kí",
+	   command = DK
 	   )
 	Regis.grid(row=6,column=0,pady = 3)
-
-
-
-
+	newwin.mainloop()
 
 def Connect():
 	global client
@@ -61,16 +228,10 @@ def Connect():
 	try:
 	    client.connect(server_address)
 	    messagebox.showinfo("Kết nối", "Bạn đã kết nối thành công!!!")
-	    Connected()
 	    mainwin.destroy()
+	    Connected()
 	except:
 	    messagebox.showinfo("Kết nối", "Sai IP")
-
-def SendMess():
-	client.sendall(bytes("Hi","utf8"))
-	rep = client.recv(1024).decode("utf8")
-	print(rep)
-
 
 mainwin = Tk()
 mainwin.title("CLIENT")
--- Server.py
+++ Server.py
@@ -17,11 +17,69 @@ def Server_Running():
     print(HOST+':'+str(PORT))
 
     def SendMess(client, adr):
+
+        #function cho server:
+
+        def DangKi():
+            Username = client.recv(1024).decode("utf8")
+            client.sendall(bytes("Da nhan username", "utf8"))
+
+            file = open("Database.txt", 'r')
+            while True:
+                line = file.readline()
+                line = line.strip()
+                if not line: break
+                UP = line.split(" ", 2)
+                if UP[0] == Username:
+                    client.sendall(bytes("ExistUsername", "utf8"))
+                    check = client.recv(1024).decode("utf8")
+                    return
+            client.sendall(bytes("ValidUsername", "utf8"))
+            check = client.recv(1024).decode("utf8")
+            file.close()
+
+            Password = client.recv(1024).decode("utf8")
+            client.sendall(bytes("Da nhan password","utf8"))
+
+            file = open("Database.txt", 'a')
+            file.write('\n')
+            file.write(Username + ' ' + Password)
+        
+        def DangNhap():
+            Username = client.recv(1024).decode("utf8")
+            client.sendall(bytes("Da Nhan username", "utf8"))
+
+            Password = client.recv(1024).decode("utf8")
+            client.sendall(bytes("Da Nhan Password", "utf8"))
+
+            file = open("Database.txt", 'r')
+            while True:
+                line = file.readline()
+                line = line.strip()
+                if not line: break
+                UP = line.split(" ", 2)
+                if UP[0] == Username and UP[1] != Password:
+                    client.sendall(bytes("WrongPass", "utf8"))
+                    check = client.recv(1024).decode("utf8")
+                    return
+                if UP[0] == Username and UP[1] == Password:
+                    client.sendall(bytes("RightUser", "utf8"))
+                    check = client.recv(1024).decode("utf8")
+                    return
+            client.sendall(bytes("WrongUser", "utf8"))
+            check = client.recv(1024).decode("utf8")
+            file.close()
+
+
         #Command cho server:
         while True:
-            data = client.recv(1024).decode("utf8")
+
+            command = client.recv(1024).decode("utf8")
             client.sendall(bytes("Da Nhan","utf8"))
-            print(data)
+            print(command)
+            
+            if command == "DangKi": DangKi()
+            elif command == "DangNhap": DangNhap()
 
     while True:
         client, addr = server.accept()
